f4f0aa13526188a2ed149ad9f1c27913116ee76d
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2026-03-17 20:58:03 -0300

    chore: gitignore miniz-rs target directory

--- tests/fixtures/miniz/rust_output/target/debug/deps/miniz_rs-e243c477cffebbb6.d
+++
@@ -1,9 +0,0 @@
-/home/marche/noricum/tests/fixtures/miniz/rust_output/target/debug/deps/miniz_rs-e243c477cffebbb6.d: src/lib.rs src/tinfl.rs src/tdef.rs src/zip.rs src/facade.rs
-
-/home/marche/noricum/tests/fixtures/miniz/rust_output/target/debug/deps/libminiz_rs-e243c477cffebbb6.rmeta: src/lib.rs src/tinfl.rs src/tdef.rs src/zip.rs src/facade.rs
-
-src/lib.rs:
-src/tinfl.rs:
-src/tdef.rs:
-src/zip.rs:
-src/facade.rs:
